3a36fc4c69409581695f10fedf086b344c41a68d
Goundla Saivaraprasad Goud <g.saivar9399@gmail.com>
2026-02-06 23:21:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let temperatur = document.querySelector(".temperature");
+let lcntn = document.querySelector(".location");
+let cndn = document.querySelector(".condition");
+let btn = document.querySelector(".search_btn");
+let date_time = document.querySelector(".date_time");
+let img = document.querySelector("img");
+let form = document.querySelector("form");
+
+let Location = 'Hyderabad';
+
+
+
+form.addEventListener('click',searchforlocation);
+
+
+
+
+const fetchdata = async(Location) =>{
+    let url = `http://api.weatherapi.com/v1/current.json?key=f9fdf6925871479fa11174511260202&q=${Location}&aqi=no`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    lcntn.innerText = data.location.name;
+    date_time.innerText = data.location.localtime;
+    temperatur.innerText = data.current.temp_c + " °C";
+    cndn.innerText = data.current.condition.text;
+    img.src = data.current.condition.icon;
+
+ 
+}
+
+function searchforlocation(e){
+    e.preventDefault();
+    let TargetLocation = document.querySelector(".search_box").value;
+    fetchdata(TargetLocation);
+}
+
+
+
+fetchdata(Location);
